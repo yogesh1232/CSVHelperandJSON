17faa0bcfede11631e4483b7c712433f044ad981
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-02-14 11:31:22 +0530

    Add:CSV and JSON file operation using third party library'

---
+++ CSVHelperAndJson/Program.cs
@@ -0,0 +1,6 @@
+﻿using CSVHelperAndJson;
+
+Console.WriteLine("# Read data from CSV & Write data in CSV");
+//CSVHandler.ImplementCSVDataHandling();
+ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+Console.ReadKey();
---
+++ CSVHelperAndJson/ReadCSV_And_WriteJSON.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CsvHelper;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace CSVHelperAndJson
+{
+    class ReadCSV_And_WriteJSON
+    {
+        public static void ImplementCSVToJSON()
+        {
+            string importFilePath = @"D:\Fellowship\Fellowship05-Day21_ThirdPartyLibrary\Day21_CSVHelperAndJson\Day21_CSVHelperAndJson\Utility\Addresses.csv";
+            string exportFilePath = @"D:\Fellowship\Fellowship05-Day21_ThirdPartyLibrary\Day21_CSVHelperAndJson\Day21_CSVHelperAndJson\Utility\export.csv";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from addresses csv.");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("\t" + addressData.firstname);
+                    Console.Write("\t" + addressData.lastname);
+                    Console.Write("\t" + addressData.address);
+                    Console.Write("\t" + addressData.city);
+                    Console.Write("\t" + addressData.state);
+                    Console.Write("\t" + addressData.code);
+                    Console.WriteLine();
+                }
+                Console.WriteLine("**********************Reading from csv file and Write to csv file **************************");
+
+                //Writing json file
+
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportFilePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+
+            }
+        }
+    }
+
+}
